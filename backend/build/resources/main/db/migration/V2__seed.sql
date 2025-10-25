597a01aaa1a8d1296f9da96c194f5a8c73763018
Charles Hayes <cfhayes16@gmail.com>
2025-10-24 22:09:35 -0600

    Delete backend/build directory

--- backend/build/resources/main/db/migration/V2__seed.sql
+++
@@ -1,6 +0,0 @@
--- PostgreSQL seed data
-INSERT INTO flights (origin, destination, departure_date, return_date, passengers, travel_class)
-VALUES
-  ('San Francisco (SFO)', 'Taipei (TPE)', '2020-01-09', '2020-01-12', 1, 'Economy'),
-  ('New York (JFK)',      'London (LHR)', '2020-02-15', '2020-02-22', 2, 'Business'),
-  ('Los Angeles (LAX)',   'New York (JFK)', '2020-01-15', '2020-01-19', 1, 'Premium Economy');
